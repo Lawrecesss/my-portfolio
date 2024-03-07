5b10d12715cab66ec6d102f922a6c4a5adcfaa55
Lawrence <lhshein14@gmail.com>
2024-03-07 22:34:28 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import AboutMe from "./components/AboutMe";
-import Contact from "./components/Contact/Contact";
-import Home from "./components/Home";
 import NavBar from "./components/Nav/NavBar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
 import { ThemeProvider } from "./components/Theme/ThemeProvider";
 
 const App = () => {
   return (
     <ThemeProvider>
       <NavBar />
-      <Home />
-      <AboutMe />
-      <Skills />
-      <Projects />
-      <Contact />
     </ThemeProvider>
   );
 };
